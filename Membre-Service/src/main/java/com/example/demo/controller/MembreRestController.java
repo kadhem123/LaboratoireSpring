7cbbca6a7410df8e4aa7e7c06dc46463bea666b7
kadhembelghuith <kadhembelghuith@ieee.org>
2020-12-25 22:27:43 +0100

    second commit

--- Membre-Service/src/main/java/com/example/demo/controller/MembreRestController.java
+++ Membre-Service/src/main/java/com/example/demo/controller/MembreRestController.java
@@ -7,13 +7,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.PublicationBean;
 import com.example.demo.entities.EnseignantChercheur;
@@ -60,7 +55,11 @@ public class MembreRestController {
 		p.setId(id);
 		return iMemberService.updateMember(p);
 	}
+	@DeleteMapping(value = "/members/member/{id}")
+	public void deleteMember(@PathVariable Long id){
+	 iMemberService.deleteMember(id);
 
+	}
 	@PutMapping(value="/membres/enseignant/{id}")
 	public Membre updateMembre(@PathVariable Long id, @RequestBody EnseignantChercheur p)
 	{
